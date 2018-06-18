51dd0a22aaffc93a7735f0649f1c01c06fe6214c
Gaston Snaider <gnsnaider@gmail.com>
2018-06-18 00:51:32 -0300

    Exercise 3 multiple letters

--- tp/exercises/ex3.py
+++ tp/exercises/ex3.py
@@ -3,63 +3,68 @@ import matplotlib.pyplot as plt
 
 from scipy.io import wavfile as wav
 
-from util.input import A_PERIOD_START, A_PERIOD_END, I_PERIOD_START, I_PERIOD_END
+from util.input import A_PERIOD_START, A_PERIOD_END, I_PERIOD_START, I_PERIOD_END, O_PERIOD_START, O_PERIOD_END
 from util.signal import sub_signal, plot_fourier_coefficients
 
 
-def exercise(x, fs, output, threshold=None):
-
-    # Obtenemos un periodo de una 'A'
-    x_a, _ = sub_signal(x, fs, A_PERIOD_START, A_PERIOD_END)
+def fourier_and_rebuild(x, fs, letter, start, end, threshold=None):
+    x_sub, _ = sub_signal(x, fs, start, end)
 
     # Calculamos los coeficientes de Fourier
-    X_a = np.fft.fft(x_a) / len(x_a)
+    X = np.fft.fft(x_sub) / len(x_sub)
 
     if (threshold != None):
         # Pasamos a cero los coeficientes menores al umbral
-        X_a[np.absolute(X_a) < threshold] = 0
-        plot_fourier_coefficients(X_a, "Coeficientes de Fourier truncados de periodo de 'A'")
+        X[np.absolute(X) < threshold] = 0
+        plot_fourier_coefficients(X, "Coeficientes de Fourier truncados de periodo de '%s'" % letter)
 
     # Reconstruimos la señal a partir de sus coeficientes
-    x_a_rebuilt = np.real(np.fft.ifft(X_a))
+    x_rebuilt = np.real(np.fft.ifft(X))
 
     # Periodizamos la señal reconstruida y la guardamos como un audio
-    x_a_periodic = np.squeeze(np.tile(x_a_rebuilt, [1, 500]))
-    wav.write(output, fs, x_a_periodic)
+    x_periodic = np.squeeze(np.tile(x_rebuilt, [1, 500]))
 
-    return x_a_rebuilt, x_a_periodic
+    return x_rebuilt, x_periodic
 
-if __name__ == "__main__":
-    (fs, x) = wav.read("../data/hh15.WAV")
 
-    x_a, x_a_periodic = exercise(x,fs,"../data/A1.wav")
-    x_a2, x_a2_periodic = exercise(x, fs,"../data/A2.wav", threshold=100)
-
+def exercise(x, fs, letter, period_start, period_end, threshold):
+    x_period, x_periodic = fourier_and_rebuild(x, fs, letter, period_start, period_end)
+    x2_period, x2_periodic = fourier_and_rebuild(x, fs, letter, period_start, period_end, threshold)
 
     # Graficamos las señales reconstruidas
     plt.figure()
     plt.grid(linestyle='dashed')
-    plt.title("Periodos de 'A' reconstruidos")
+    plt.title("Periodos de '%s' reconstruidos" % letter)
     plt.xlabel("Tiempo [s]")
     plt.ylabel("Señal")
-    t = np.arange(len(x_a)) / fs
-    plt.plot(t, x_a)
-    plt.plot(t, x_a2)
-    plt.legend(["'A' con coeficientes originales","'A' con coeficientes truncados"])
+    t = np.arange(len(x_period)) / fs
+    plt.plot(t, x_period)
+    plt.plot(t, x2_period)
+    plt.legend(["'%s' con coeficientes originales" % letter, "'%s' con coeficientes truncados" % letter])
     plt.show()
-
 
     plt.figure()
     plt.grid(linestyle='dashed')
-    plt.title("Señales de 'A' periodizadas")
+    plt.title("Señales de '%s' periodizadas" % letter)
     plt.xlabel("Tiempo [s]")
     plt.ylabel("Señal")
-
     periods = 4
-    samples = len(x_a) * periods
+    samples = len(x_period) * periods
     t = np.arange(samples) / fs
-    plt.plot(t, x_a_periodic[:samples])
-    plt.plot(t, x_a2_periodic[:samples])
-
-    plt.legend(["'A' con coeficientes originales", "'A' con coeficientes truncados"])
+    plt.plot(t, x_periodic[:samples])
+    plt.plot(t, x2_periodic[:samples])
+    plt.legend(["'%s' con coeficientes originales" % letter, "'%s' con coeficientes truncados" % letter])
     plt.show()
+
+    output1 = "../data/%s1.wav" % letter
+    output2 = "../data/%s2.wav" % letter
+    wav.write(output1, fs, x_periodic)
+    wav.write(output2, fs, x2_periodic)
+
+
+if __name__ == "__main__":
+    (fs, x) = wav.read("../data/hh15.WAV")
+
+    exercise(x, fs, 'A', A_PERIOD_START, A_PERIOD_END, threshold=50)
+    # exercise(x, fs, 'O', O_PERIOD_START, O_PERIOD_END, threshold=250)
+    # exercise(x, fs, 'I', I_PERIOD_START, I_PERIOD_END, threshold=200)
