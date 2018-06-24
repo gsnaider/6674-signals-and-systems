3922e8bab8e98df21f30f49abd1ec64ff36a7251
Gaston Snaider <gnsnaider@gmail.com>
2018-06-24 15:09:46 -0300

    Exs 6 and 7

--- tp/exercises/ex6.py
+++ tp/exercises/ex6.py
@@ -5,7 +5,8 @@ from util.signal import glottal_pulse
 import matplotlib.pyplot as plt
 
 if __name__ == "__main__":
-    x, t = glottal_pulse(f0=200, Tp_pct=0.4, Tn_pct=0.16, P0=1, periods=10, fs=16000)
+    fs = 16000
+    x, t = glottal_pulse(f0=200, Tp_pct=0.4, Tn_pct=0.16, P0=1, periods=10, fs=fs)
 
     plt.figure()
     plt.plot(t,x)
@@ -16,7 +17,25 @@ if __name__ == "__main__":
     plt.show()
 
     amplitude_spectrum = abs(np.fft.fft(x) / len(x))
+    freqs = np.arange(0, fs/2, (fs/2) / len(amplitude_spectrum))
 
     plt.figure()
-    plt.plot(amplitude_spectrum)
+    plt.title("Espectro de amplitud de tren de pulsos")
+    plt.xlabel("Frecuencia [Hz]")
+    plt.ylabel("Amplitud")
+    plt.plot(freqs, amplitude_spectrum)
+    plt.show()
+
+    # Comparamos el espectro de un pulso con el del tren de pulsos
+    pulse = x[:len(x) // 10]
+    pulse_amp_spec = abs(np.fft.fft(pulse) / len(pulse))
+    pulse_freqs = np.arange(0, fs / 2, (fs / 2) / len(pulse_amp_spec))
+
+    plt.figure()
+    plt.plot(freqs, amplitude_spectrum)
+    plt.plot(pulse_freqs, pulse_amp_spec)
+    plt.title("Espectros de amplitud")
+    plt.xlabel("Frecuencia [Hz]")
+    plt.ylabel("Amplitud")
+    plt.legend(["Espectro de amplitud del tren de pulsos", "Espectro de amplitud de un puslo"])
     plt.show()
