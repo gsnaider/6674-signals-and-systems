54bd2825dbfacc69db83a3822cc922176fbcabaf
Gaston Snaider <gnsnaider@gmail.com>
2018-06-25 15:55:01 -0300

    Add signal threshold

--- tp2/exercises/ex10.py
+++ tp2/exercises/ex10.py
@@ -2,6 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 from scipy.io import wavfile as wav
 
+from util.input import letters
 from util.speech import fundamental_freq
 
 if __name__ == "__main__":
@@ -17,12 +18,16 @@ if __name__ == "__main__":
     segments = np.array_split(x, n_segments)
     f0s = []
     for segment in segments:
-        f0s.append(fundamental_freq(segment, fs))
+        if np.max(np.abs(segment)) > 200:
+            f0s.append(fundamental_freq(segment, fs))
+        else:
+            f0s.append(0)
 
     f0s = np.array(f0s)
-
     f0s = np.repeat(f0s, window_size)
-    f0s = np.pad(f0s, (0, len(t) - len(f0s)), 'constant', constant_values=(0, 0))
+
+    # Padding al final de f0s para que coincida con len(t)
+    f0s = np.pad(f0s, (0, len(t) - len(f0s)), 'edge')
 
 
     plt.figure()
@@ -32,6 +37,13 @@ if __name__ == "__main__":
     plt.title("Señal de voz")
     plt.xlabel("Tiempo [s]")
     plt.ylabel("Señal x")
+    for letter in letters:
+        offset = 0.015
+        letter_x = (letter.start + letter.end) / 2 - offset
+        letter_y = 20000
+        plt.text(letter_x, letter_y, letter.char, fontsize=12)
+        plt.axvline(x=letter.start, color='r', linestyle='dotted')
+        plt.axvline(x=letter.end, color='r', linestyle='dotted')
 
 
     plt.subplot(2, 1, 2)
